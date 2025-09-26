d3b0af0798254e2c8b035a0101d7bdd21bb7b311
LI <markiana@mail.ru>
2025-09-26 22:50:50 +0500

    les2_2 add

--- app/src/main/kotlin/LessoN_2/lesson2_2.kt
+++ app/src/main/kotlin/LessoN_2/lesson2_2.kt
@@ -5,9 +5,13 @@ fun main() {
     val thankYouMessage: String = "Spasibo za pokupku"
     println(orderCount)
     println(thankYouMessage)
-    var employeeCount = 2000
-    println("Kolichestvo rabotnikov: $employeeCount")
 
+    var employeeCount = 2000
+    println("Kol-vo rabotnikov: $employeeCount")
+
+    // Представим, что один работник уволился
+    // println("Количество работников: $employeeCount")  // ← старый вывод закомментирован
     employeeCount = 1999
-    println("Kolichestvo rabotnikov posle yvolneniya: $employeeCount")
+    println("Kol-vo rabotnikov poske yvolneniya: $employeeCount")
+
 }
