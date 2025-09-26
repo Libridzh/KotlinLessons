6c21e22bb03d2a7d344ec6ee8d7802313cf24ee4
LI <markiana@mail.ru>
2025-09-26 22:32:25 +0500

    les2_2 add

---
+++ app/src/main/kotlin/LessoN_2/lesson2_2.kt
@@ -0,0 +1,13 @@
+package org.example.app.LessoN_2
+
+fun main() {
+    val orderCount: Int = 0
+    val thankYouMessage: String = "Spasibo za pokupku"
+    println(orderCount)
+    println(thankYouMessage)
+    var employeeCount = 2000
+    println("Kolichestvo rabotnikov: $employeeCount")
+
+    employeeCount = 1999
+    println("Kolichestvo rabotnikov posle yvolneniya: $employeeCount")
+}
