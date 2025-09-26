e70ce5a68937d69febf5e845d9349b2f599262b9
LI <markiana@mail.ru>
2025-09-26 21:37:27 +0500

    les2_1 add

---
+++ app/src/main/kotlin/LessoN_2/lesson2_1.kt
@@ -0,0 +1,9 @@
+package org.example.app.LessoN_2
+
+fun main() {
+    var order_count: Int = 0
+    val thank_you_message: String = "Spasibo za pokupku"
+    println(order_count)
+    println(thank_you_message)
+
+}
