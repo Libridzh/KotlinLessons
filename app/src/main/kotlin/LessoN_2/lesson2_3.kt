87d0356f307d2cc1fe2c869043e2cd4c854244ed
LI <markiana@mail.ru>
2025-09-27 13:16:30 +0500

    les2_3 add

---
+++ app/src/main/kotlin/LessoN_2/lesson2_3.kt
@@ -0,0 +1,24 @@
+package org.example.app.LessoN_2
+
+fun main() {
+    // Год полёта неизменен → val
+    val year = 1961
+
+    // Часы и минуты меняются (сначала взлёт, потом посадка) → var
+    var hour = 9
+    var minute = 7
+
+    // Вывод по одной переменной на строку
+    println(year)
+    println(hour)
+    println(minute)
+
+    // Изменяем значения для времени посадки
+    hour = 10
+    minute = 55
+
+    // Вывод времени посадки в формате [часы:минуты]
+    print(hour)      // print без переноса строки
+    print(":")
+    println(minute)  // println завершает строку
+}
